bfd6a88e9015a18572b81684649c0b40948fb6c3
dbsgmlrua <dbsgmlrua2@gmail.com>
2021-07-15 21:10:06 +0900

    serializer삭

--- backend/stock/serializers.py
+++
@@ -1,4 +0,0 @@
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
-from rest_framework import serializers
-class RunningCheckerSerializer(serializers.Serializer):
-    running = serializers.BooleanField()
