994c66a6540aa6479e43c10e7676d31f762dc107
dbsgmlrua <dbsgmlrua2@gmail.com>
2021-07-29 22:38:05 +0900

    왜 밤중에 일시키지

--- backend/stock/serializers.py
+++ backend/stock/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
 from .models import Stocks
 
-
 class HtsStarterSerializer(serializers.Serializer):
     start = serializers.CharField(max_length=10)
 
@@ -13,6 +12,12 @@ class BalanceSerializer(serializers.Serializer):
     name = serializers.CharField(max_length=10)
     qty = serializers.IntegerField()
     yd = serializers.DecimalField()
+
+class MarketSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    code = serializers.CharField(max_length=10)
+    name = serializers.CharField(max_length=10)
+    stdPrice = serializers.CharField(max_length=30)
 
 class StocksSerializer(serializers.ModelSerializer):
     class Meta:
