c73f2379354965bfe807c5bd797530b9227a51a5
dbsgmlrua <dbsgmlrua2@gmail.com>
2021-09-03 22:09:55 +0900

    tmp

--- backend/stock/classes/test/TestCodeDummy.py
+++ backend/stock/classes/test/TestCodeDummy.py
@@ -1,5 +1,4 @@
 from stock.classes.exceptions.StockExceptionHandler import raise_exception_by_errorcode, ErrorCode
-
 def ExceptionTestChecker(code):
     if True:
         return raise_exception_by_errorcode(ErrorCode.NOT_CONNECTED)
