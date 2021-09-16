3fe6533f64e2545e7115073d5baf78ed7cb58a50
dbsgmlrua <dbsgmlrua2@gmail.com>
2021-09-16 20:27:27 +0900

    getStockDetail

--- backend/stock/classes/CreonStockDetail.py
+++ backend/stock/classes/CreonStockDetail.py
@@ -28,4 +28,23 @@ class CreonStockDetail(metaclass=Singleton):
         return stocklist
     
     def get_stock_detail(self, code):
-        return None
+        creonClients = CreonClients()
+        objStockMst = getattr(creonClients,'StockMst')
+        objStockMst.BlockRequest()
+
+        code = objStockMst.GetHeaderValue(0)  # 종목코드
+        name = objStockMst.GetHeaderValue(1)  # 종목명
+        time = objStockMst.GetHeaderValue(4)  # 시간
+        cprice = objStockMst.GetHeaderValue(11)  # 종가
+        diff = objStockMst.GetHeaderValue(12)  # 대비
+        op = objStockMst.GetHeaderValue(13)  # 시가
+        high = objStockMst.GetHeaderValue(14)  # 고가
+        low = objStockMst.GetHeaderValue(15)  # 저가
+        offer = objStockMst.GetHeaderValue(16)  # 매도호가
+        bid = objStockMst.GetHeaderValue(17)  # 매수호가
+        vol = objStockMst.GetHeaderValue(18)  # 거래량
+        vol_value = objStockMst.GetHeaderValue(19)  # 거래대금
+
+        stockDetail = StockDetail(code, name, time, cprice, diff, op, high, low, offer, bid, vol, vol_value)
+        
+        return stockDetail 
